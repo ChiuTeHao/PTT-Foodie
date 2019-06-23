38d9d8e2ed5c5541fbbe0b1b819896bf2ca19b14
MichaelChiu <r07944002@ntu.edu.tw>
2019-06-23 14:14:22 +0800

    modify star score if store have no comment

--- src/searchengine.py
+++ src/searchengine.py
@@ -90,12 +90,14 @@ class SearchEngine:
                         scores[docname]+=idf*tf
         tmpresults=sorted(scores.items(),key=lambda x:x[1],reverse=True)
         maxscore=tmpresults[0][1]
-        base=tmpresults[39][1]
+        last=min(40,len(tmpresults))
+        base=tmpresults[last][1]
         results=[]
         for tmpresult in tmpresults:
             results.append((tmpresult[0],(tmpresult[1]-base)/(maxscore-base)))
         return results
     def rank(self,query):
+        print(len(query))
         queryword=self.cutQuery(query)
         mainloc,miniloc=self.parseLocation(query)
         ranklist=[]
@@ -113,15 +115,26 @@ class SearchEngine:
                 locscore+=5
             if 'stars' in self.doclist[idx] and self.doclist[idx]['stars']>=0:
                 starscore=self.doclist[idx]['stars']
-            if 'store_name' in self.doclist[idx] and query in self.doclist[idx]['store_name']:
+            else:
+                starscore=4.05
+            if query in self.doclist[idx]['article_title'] or ('store_name' in self.doclist[idx]  and query in self.doclist[idx]['store_name']):
                 storenamescore=1
-            results2.append((result[0],result[1]+locscore+starscore/5+storenamescore,storenamescore))
+            results2.append((result[0],result[1]+locscore+starscore/5+storenamescore,result[1],locscore,starscore,storenamescore))
         results2=sorted(results2,key=lambda x:x[1],reverse=True)[:10]
-        self.printResult(results[:10],True)
-        print('=================================')
-        self.printResult(results2[:10],True)
-        result3=[]
-        for result in results2:
-            idx=self.docidxtable[result[0]]
-            result3.append(self.doclist[idx])
-        return result3
+        #self.printResult(results[:20],True)
+        #print('=================================')
+        #self.printResult(results2[:10],True)
+        results3=[]
+        for r in results[:20]:
+            idx=self.docidxtable[r[0]]
+            if 'stars' not in self.doclist[idx] or self.doclist[idx]['stars']==-5.0:
+                self.doclist[idx]['stars']=4.05
+        for r in results2:
+            idx=self.docidxtable[r[0]]
+            if 'stars' not in self.doclist[idx] or self.doclist[idx]['stars']==-5.0:
+                self.doclist[idx]['stars']=4.05
+            results3.append(self.doclist[idx])
+        self.printResult(results[:20],True)
+        print('================================')
+        self.printResult(results2,True)
+        return results3
